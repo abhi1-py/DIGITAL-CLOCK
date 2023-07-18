42f6f9a081e44a180ca427cfda5b9c0c64a2449f
abhi1-py <120701338+abhi1-py@users.noreply.github.com>
2023-07-18 22:09:56 +0530

    clock

---
+++ src/GreetingCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css'; // Import the CSS file for styling
+
+const GreetingCard = () => {
+    return (
+        <div className="greeting-card">
+            <div className="greeting-card-content">
+                <h1 className="greeting-title"><p></p>&nbsp;&nbsp;We welcome you </h1>
+                <p className="greeting-message">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"𝐓𝐎 𝐓𝐇𝐄 𝐆𝐑𝐀𝐍𝐃 𝐎𝐏𝐄𝐍𝐈𝐍𝐆 𝐎𝐅"<br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;𝑲𝑨𝑩𝑰𝑹𝑨 𝑪𝑨𝑭𝑬
+                </p>
+                <p class="text-container">
+                &nbsp;&nbsp; 𝗦𝘂𝗻𝗱𝗮𝘆, 𝗠𝗮𝗿𝗰𝗵 𝟮𝟱,𝟮𝟬𝟮𝟯<br></br>&nbsp;&nbsp;𝒂𝒕 12.01 𝑷𝑴 𝑰𝑺𝑻<br></br>&nbsp;&nbsp;𝗠𝗚 𝗥𝗼𝗮𝗱,𝗕𝗮𝗻𝗴𝗮𝗹𝗼𝗿𝗲
+                </p>
+                {/* <p class="image-container">ISRO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p> */}
+                 
+            </div>
+        </div>
+    );
+};
+
+export default GreetingCard;
+
+
+// We are thrilled to have you here as we embark on a new journey of exploration and discovery.
+// Chandrayaan 3, the next chapter in India's space exploration, is set to inspire and push the boundaries of human knowledge.<br>
+//         </br>
+// Join us as we witness the awe-inspiring launch, where science, technology, and innovation converge to reach for the stars. Let's celebrate this remarkable achievement and the dedicated efforts of the brilliant minds behind it.<br>
+// </br>
+// Get ready to witness history in the making!
